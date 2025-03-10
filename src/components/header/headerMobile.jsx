b8642bf202b5d347e0fa902a5bfea274a5233158
mkh-web <mis.mdev@gmail.com>
2025-03-10 07:11:35 -0700

    fixing

--- src/components/header/headerMobile.jsx
+++ src/components/header/headerMobile.jsx
@@ -15,6 +15,14 @@ export default function Headermobile() {
                     <div className={`w-6 h-1 bg-orange-500 rounded-sm ${isOpen ? "opacity-0" : ""} ease-in-out duration-500`} />
                     <div className={`w-6 h-1 bg-orange-500 rounded-sm ${isOpen ? "-rotate-45" : ""} origin-left ease-in-out duration-500`} />
                </div>
+
+         {/*       {NavbarList.list.map((links) => (
+                    <Link href={links.pathname} key={links.title} className={`${pathname === links.pathname && "bg-white text-[#171717] rounded-lg !opacity-100"}
+                            flex justify-between items-center w-full p-5 flex-row`}>
+                         {links.title}
+                         {links.icon}
+                    </Link>
+               ))} */}
           </div>
      )
 
--- src/components/serv.jsx
+++ src/components/serv.jsx
@@ -1,4 +1,4 @@
-import { MdOutlineDevices,MdSell ,MdOutlineAnalytics  } from "react-icons/md";
+import { MdOutlineDevices, MdSell, MdOutlineAnalytics } from "react-icons/md";
 import { SiAdobecreativecloud } from "react-icons/si";
 import { AiOutlineFundProjectionScreen } from "react-icons/ai";
 import { IoMdDownload } from "react-icons/io";
@@ -23,15 +23,15 @@ export default function Serv() {
                     </div>
 
                     <div className="flexCenter flex-col w-[300px] h-[250px] px-2
-                    relative bg-white/10 backdrop-blur-md rounded-xl border hover:border-white/20 hover:-translate-y-3 transition-all shadow-lg p-6">                    
-                        <SiAdobecreativecloud className=" text-[50px] mb-5"/>
+                    relative bg-white/10 backdrop-blur-md rounded-xl border hover:border-white/20 hover:-translate-y-3 transition-all shadow-lg p-6">
+                         <SiAdobecreativecloud className=" text-[50px] mb-5" />
                          <h1 className="font-bold text-2xl"> التصميم الإبداعي</h1>
                          <h1>نقوم بتصميم وتطوير مواقع الويب والتطبيقات بأحدث التقنيات وأفضل الممارسات لضمان تجربة مستخدم مميزة وأداء عالي.</h1>
                     </div>
 
                     <div className="flexCenter flex-col w-[300px] h-[250px] px-2
-                    relative bg-white/10 backdrop-blur-md rounded-xl border hover:border-white/20 hover:-translate-y-3 transition-all shadow-lg p-6">                         
-                    <MdSell  className=" text-[50px] mb-5"/>
+                    relative bg-white/10 backdrop-blur-md rounded-xl border hover:border-white/20 hover:-translate-y-3 transition-all shadow-lg p-6">
+                         <MdSell className=" text-[50px] mb-5" />
                          <h1 className="font-bold text-2xl">التسويق الرقمي </h1>
                          <h1>
                               نقدم خدمات تصميم إبداعية تشمل تصميم الهوية البصرية والشعارات والمواد التسويقية بأسلوب عصري ومبتكر.
@@ -39,8 +39,8 @@ export default function Serv() {
                     </div>
 
                     <div className="flexCenter flex-col w-[300px] h-[250px] px-2
-                    relative bg-white/10 backdrop-blur-md rounded-xl border hover:border-white/20 hover:-translate-y-3 transition-all shadow-lg p-6">                         
-                    <MdOutlineAnalytics  className=" text-[50px] mb-5"/>
+                    relative bg-white/10 backdrop-blur-md rounded-xl border hover:border-white/20 hover:-translate-y-3 transition-all shadow-lg p-6">
+                         <MdOutlineAnalytics className=" text-[50px] mb-5" />
                          <h1 className="font-bold text-2xl"> تحليل البيانات </h1>
                          <h1>
                               نقدم خدمات تحليل البيانات واستخراج الرؤى والتوصيات لاتخاذ القرارات الصحيحة وتحسين الأداء وتحقيق النتائج المرجوة.
@@ -48,8 +48,8 @@ export default function Serv() {
                     </div>
 
                     <div className="flexCenter flex-col w-[300px] h-[250px] px-2
-                    relative bg-white/10 backdrop-blur-md rounded-xl border hover:border-white/20 hover:-translate-y-3 transition-all shadow-lg p-6">                         
-                    <AiOutlineFundProjectionScreen className=" text-[50px] mb-5"/>
+                    relative bg-white/10 backdrop-blur-md rounded-xl border hover:border-white/20 hover:-translate-y-3 transition-all shadow-lg p-6">
+                         <AiOutlineFundProjectionScreen className=" text-[50px] mb-5" />
                          <h1 className="font-bold text-2xl">مشاريع تخرج + كبيرة</h1>
                          <h1>
                               استراتيجيات تسويقية متكاملة لتعزيز تواجدكم الرقمي وزيادة الانتشار وتحقيق أهدافكم التسويقية بكفاءة عالية.
@@ -57,8 +57,8 @@ export default function Serv() {
                     </div>
 
                     <div className="flexCenter flex-col w-[300px] h-[250px] px-2
-                    relative bg-white/10 backdrop-blur-md rounded-xl border hover:border-white/20 hover:-translate-y-3 transition-all shadow-lg p-6">                         
-                    <TbReportSearch className=" text-[50px] mb-5"/>
+                    relative bg-white/10 backdrop-blur-md rounded-xl border hover:border-white/20 hover:-translate-y-3 transition-all shadow-lg p-6">
+                         <TbReportSearch className=" text-[50px] mb-5" />
                          <h1 className="font-bold text-2xl"> الاستشارات والتدريب</h1>
                          <h1>
                               نقدم استشارات متخصصة وبرامج تدريبية لتطوير المهارات وتحسين الأداء ومواكبة التطورات في مختلف المجالات.
@@ -66,8 +66,8 @@ export default function Serv() {
                     </div>
 
                     <div className="flexCenter flex-col w-[300px] h-[250px] px-2
-                    relative bg-white/10 backdrop-blur-md rounded-xl border hover:border-white/20 hover:-translate-y-3 transition-all shadow-lg p-6">                         
-                    <IoMdDownload className=" text-[50px] mb-5"/>
+                    relative bg-white/10 backdrop-blur-md rounded-xl border hover:border-white/20 hover:-translate-y-3 transition-all shadow-lg p-6">
+                         <IoMdDownload className=" text-[50px] mb-5" />
                          <h1 className="font-bold text-2xl">تحميل عن طريق الانترنت</h1>
                          <h1>
                               نقدم خدمة التحميل حسب الطلب عن طريق الانترنت بأسعار منافسة للسوق وحتى الخدمات الحكومية!
@@ -75,8 +75,8 @@ export default function Serv() {
                     </div>
 
                     <div className="flexCenter flex-col w-[300px] h-[250px] px-2
-                    relative bg-white/10 backdrop-blur-md rounded-xl border hover:border-white/20 hover:-translate-y-3 transition-all shadow-lg p-6">                         
-                    <CiRouter className=" text-[50px] mb-5"/>
+                    relative bg-white/10 backdrop-blur-md rounded-xl border hover:border-white/20 hover:-translate-y-3 transition-all shadow-lg p-6">
+                         <CiRouter className=" text-[50px] mb-5" />
                          <h1 className="font-bold text-2xl"> الشبكات </h1>
                          <h1>
                               تركيب وبرمجة الروترات والمايكروتيك ومختلف اجهزة الشبكات باسعار غير مسبوقة.
@@ -84,8 +84,8 @@ export default function Serv() {
                     </div>
 
                     <div className="flexCenter flex-col w-[300px] h-[250px] px-2
-                    relative bg-white/10 backdrop-blur-md rounded-xl border hover:border-white/20 hover:-translate-y-3 transition-all shadow-lg p-6">                         
-                    <FaDatabase className=" text-[50px] mb-5"/>
+                    relative bg-white/10 backdrop-blur-md rounded-xl border hover:border-white/20 hover:-translate-y-3 transition-all shadow-lg p-6">
+                         <FaDatabase className=" text-[50px] mb-5" />
                          <h1 className="font-bold text-xl"> تصميم قواعد البيانات ورسوماتها البيانية </h1>
                          <h1>
                               تصميم قواعد بيانات وقواعد معرفة مناسبة لرغبات واعمال عملائنا سواء كانت عن طريق sql, excel او غيره.
@@ -94,7 +94,7 @@ export default function Serv() {
 
                </div>
 
-
+              
 
           </div>
      )
